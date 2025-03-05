67e0c440f79474ac7a39638325c66b274a876acf
Niinyah <petermarbell@gmail.com>
2025-03-05 09:53:42 +0100

    Updated main.java with a new change

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Github");
-        System.out.println("Version 2");
+        System.out.println("Hello Githubbers");
+        System.out.println("Version 5");
+        System.out.println("seirjsor");
     }
 }
