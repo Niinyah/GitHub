fc370d71a17f3d12933c9421dbfe15c4d74bc5d4
Niinyah <petermarbell@gmail.com>
2025-03-05 10:19:17 +0100

    Added change

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Version 5");
         System.out.println("seirjsor");
         System.out.println("Philip har tilføjet");
-        System.out.println("Peter har tilføjet");
+        System.out.println("Peter har tilføjet min rettelse er korrekt");
 
     }
 }
